0bb48cb6954223bcfda108b119db32553fa07fae
guillaume iter <guillaume.saintetienne@iter.org>
2024-01-24 18:30:59 +0100

    refacto avec Select/Map

--- MonadsFromTheTrenches/tests/simplerCase/TransformInputToListOfNumbers.cs
+++ MonadsFromTheTrenches/tests/simplerCase/TransformInputToListOfNumbers.cs
@@ -12,14 +12,9 @@ public class TransformInputToListOfNumbers
 
     public ImmutableList<Option<int>> MonadicTransform(ImmutableList<string> thatList)
     {
-        var temp = new List<Option<int>>();
-
-        foreach (var item in thatList)
-        {
-            temp.Add(int.TryParse(item, out var value) ? Option<int>.Some(value) : Option<int>.None);
-        }
-
-        // thatList.Select( str =>  procedure(str))
-        return temp.ToImmutableList();
+        return thatList
+            .Select(item =>
+                    int.TryParse(item, out var value) ? Option<int>.Some(value) : Option<int>.None)
+            .ToImmutableList();
     }
 }
