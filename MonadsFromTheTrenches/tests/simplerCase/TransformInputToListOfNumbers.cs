af6a39bcc6c7545dcf4e3da75782e392afc1ef65
guillaume iter <guillaume.saintetienne@iter.org>
2024-01-24 18:22:49 +0100

    refacto : DRY

--- MonadsFromTheTrenches/tests/simplerCase/TransformInputToListOfNumbers.cs
+++ MonadsFromTheTrenches/tests/simplerCase/TransformInputToListOfNumbers.cs
@@ -13,24 +13,17 @@ public class TransformInputToListOfNumbers
     public ImmutableList<Option<int>> MonadicTransform(ImmutableList<string> thatList)
     {
         var temp = new List<Option<int>>();
-        
+
         foreach (var item in thatList)
         {
-            if (item == null)
-            {
-                temp.Add(Option<int>.None);
-            }
+            var result = int.TryParse(item, out var value);
+            if (result)
+                temp.Add(Option<int>.Some(value));
             else
-            {
-                var result = int.TryParse(item, out var value) ;
-                if (result)
-                    temp.Add(Option<int>.Some(value));
-                else
-                    temp.Add(Option<int>.None);
-            }
+                temp.Add(Option<int>.None);
         }
-        
-       // thatList.Select( str =>  procedure(str))
-       return temp.ToImmutableList();
+
+        // thatList.Select( str =>  procedure(str))
+        return temp.ToImmutableList();
     }
 }
