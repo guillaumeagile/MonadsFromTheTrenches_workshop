f60f2891ff4409934b06ce42a140cd0bf0348693
guillaume iter <guillaume.saintetienne@iter.org>
2024-03-20 09:21:53 +0100

    live code au MTG Toulouse

--- MonadsFromTheTrenches/tests/1-simplerCase/MaybeTest.cs
+++ MonadsFromTheTrenches/tests/1-simplerCase/MaybeTest.cs
@@ -1,7 +1,7 @@
 using FluentAssertions;
 using LanguageExt;
 
-namespace MonadsFromTheTrenches.simplerCase;
+namespace MonadsFromTheTrenches.tests._1_simplerCase;
 
 public class MaybeTest
 {
@@ -10,10 +10,12 @@ public class MaybeTest
     public void Maybe_HappyPath_ProceduralWay()
     {
         // Arrange
-        var mayBeValue =  Option<int>.Some(value: 1);
+        var mayBeValue =  Option<int>.None;
     
         // Assert
-        mayBeValue.IsSome .Should().BeTrue();
+       
+        mayBeValue.IfSome( x => Assert.Fail("booo") );
+        
         // don't look into the box !!
     }
     
@@ -21,15 +23,20 @@ public class MaybeTest
     public void Maybe_HappyPath_FunctionnalWay()
     {
         // Arrange
-        var mayBeValue =  Option<int>.Some(value: 0);
+        var mayBeValue =  Option<int?>.Some(value: 0);
     
         // Assert
-        mayBeValue.IfSome(f: value => value.Should().Be(expected: 0));
+       // mayBeValue.IfSome(f: value => foo());
 
-        mayBeValue.Match(
-            Some: value => value.Should().Be(expected: 0), 
-            None: () => Assert.Fail(message: "no no no"));
+       _ = mayBeValue.Match(
+            Some: value =>  value.Should().Be(0), 
+            None: () => Assert.Fail("boo"));
     }
-    
+
+    private Action foo()
+    {
+        throw new NotImplementedException();
+    }
+
     // and now... with Cat
 }
--- MonadsFromTheTrenches/tests/2-Catz/CatzTests.cs
+++ MonadsFromTheTrenches/tests/2-Catz/CatzTests.cs
@@ -19,43 +19,52 @@ public class CatzTests
             // Arrange
             var box = Either<Error, Cat>.Right(new Cat("Fluffy", 8, 10));
 
-        box.IsRight.Should().BeFalse();
-        
         // now try to make it left/wrong
         
         box.IsLeft.Should().BeFalse();
+        
+        box.Match ( Left: err => Assert.Fail("booo"),
+            Right:  c => c.Age.Should().Be(8) );
 
         // !! 🤪 !! for the rest of the demo, we will alternate between right and left
+
+
         
         // Act
         //  //Validate, Extract, Transform, lift (if valid)
-        var expectOlder = box.Map( c => c );
+        var expectOlder = box.Map( c => c with { Age = c.Age + 1});
         expectOlder.IfRight( c => c.Age.Should().Be(9));
         
         // First, there is no difference between Map and Select.
         
-        return;
+  
         
                 //Map and Bind do much the same, however they differ only in how they return the transformed result i.e. the Lift/leave phase.
                 //In all cases the transformation function is run, however the requirements of the transformation's functions arguments
                 //and return type have changed:
 
         //  //Validate, Extract, Transform+, lift
-        var expected = box.Bind(GetsOlderIfNotTooOld);
-
+        var expected = expectOlder.Bind(GetsOlderIfNotTooOld);
         
+        expected.Match ( Left: err => err.Should().Be(new MyError("TOO_OLD")),
+            Right:  c => c.Age.Should().Be(10) );
+
+         
         //Bind requires the programmer to define and provide a transformation function that will take as input the item provided to it by the Bind function,
         //but crucially it needs to transform it in such a way that the result is a new instance of the Monad
         
         //while Map, only requires the transformation function to return the transformation from TA to TB without needing to place it into a new Box<TB>.
         
-        var expectMapped = box.Map(transformToDog);
+      //  var expectMapped = box.Map(transformToDog);
 
 
         
         
-        var expectedBindAndhange = box.Bind(OldCatBecomesDog);
+        var expectedBindAndChange = expected.Bind(OldCatBecomesDog);
         
+        expectedBindAndChange.Match ( Left: err => err.Should().Be(new MyError("sssss")),
+            Right:  c => c.Name.Should().Be("Fluffy woof!") );
+
         
         
         
@@ -64,7 +73,7 @@ public class CatzTests
         var expected2 = box.Apply(SorsDeLaVilainMatou);
         
         
-        
+           return;
         
         
         var expected3 = box.Apply(Shazzam);
@@ -83,12 +92,13 @@ public class CatzTests
 
     private Either<Error, Cat> GetsOlderIfNotTooOld(Cat arg) //Bind
     {
-        throw new NotImplementedException();
+        if (arg.Age > 10) return Either<Error, Cat>.Left(new MyError("TOO_OLD"));
+        return arg with { Age = arg.Age + 1 };
     }
     
     private Either<Error, Dog> OldCatBecomesDog(Cat arg) //Bind
     {
-        throw new NotImplementedException();
+       return new Dog( Name : arg.Name + " woof!");
     }
 
     private Cat SorsDeLaVilainMatou(Either<Error, Cat> arg) //Apply
@@ -105,22 +115,21 @@ public class CatzTests
     public void FullProcessOneSchrödingerCats()
     {
         // Arrange
-        // on a recu le chat depuis une fenetre web (DTO)
+        // on a recu le chat depuis une fenetre web (DTO) et on l'a transformé comme une entité métier de l'application.
         var inputBox = Either<Error, Cat>.Right(new Cat("Fluffy", 8, 10));
-
-        // var validated = inputBox.Bind(validate);
         
-        // var readyToBeSaved = validated.Map(transformToDTO);
-        
-        // var saved = readyToBeSaved.Bind(save);
-        
-        // var httpResponse = saved.BiMap(transformToHttpResponse);
-        
-        // httpResponse.Match( Right: x => x, Left: x => x);
+        var httpResponse = 
+        inputBox
+            .Bind(validate)  //peut nous faire passer du coté gauche si erreur de validation
+            .Map(transformToDTO)        
+            .Bind(save) //peut nous faire passer du coté gauche si erreur d'enregistrement en base de données
+            .BiMap(Left: err => new NotFoundResult(), 
+                Right: x => new OkObjectResult(x));
+            
+         httpResponse.Match( Right: x => x, Left: x => x);  // pour sortir de la Monad et ne produire que
+                                                            // un monotype: le resultat.
 
     }
 
-
-    
-
+    public Func<Cat, Either<Error, object>> validate { get; set; }
 }
